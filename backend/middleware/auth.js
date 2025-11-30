1d9ea8d16df20715fa002da56107a7b020316689
MS1098 <mayurshelar1098@gmail.com>
2025-11-30 18:17:16 +0530

    prod-1

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -7,6 +7,7 @@ module.exports = async function(req, res, next){
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findById(decoded.id).select('-password');
+    console.log("Password:",user);
     if(!user) return res.status(401).json({message:'User not found'});
     req.user = user;
     next();
